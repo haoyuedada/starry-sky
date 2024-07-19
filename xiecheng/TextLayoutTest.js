f02e3e17cee62ec0326573038390f435cb9212d3
c30053106 <chenhaoyue1@huawei.com>
2024-07-19 14:23:37 +0800

    123

---
+++ xiecheng/TextLayoutTest.js
@@ -0,0 +1,25 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function TextLayoutTest() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title} >abcde</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexWrap: 'wrap'
+  },
+  title: {
+    // 字重增大，文字框没有增大，导致后面的文字被挤走。
+    // fontWeight: '500',
+    //文字变换为大写，也会使文字变大，导致被挤出文字框。
+    textTransform:'uppercase',
+    fontSize: 40,
+    backgroundColor: 'blue'
+  }
+})
+
+export { TextLayoutTest }
